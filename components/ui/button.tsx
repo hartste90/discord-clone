93f0db2d0af1b140041e46061b8fdd74a87e268d
Steve Hart <stevenhart@ioconic.io>
2023-11-28 00:25:13 -0800

    Server Creation API

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -17,7 +17,9 @@ const buttonVariants = cva(
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",},
+        link: "text-primary underline-offset-4 hover:underline",
+        primary: "bg-indigo-500 text-white hover:bg-indigo-500/90",
+      },
       size: {
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
